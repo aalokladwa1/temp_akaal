b300ef01331693a22db175cea194dc748a94c0a9
Aalok Ladwa <aalok.ladwa@akaal.io>
2026-08-06 19:26:04 +0530

    fix(wizard): render upgraded NewMigrationWizard inside NewMigrationConfigView so UI changes are immediately visible when clicking New Migration

--- akaal_software/src/screens/MigrationModule/index.ts
+++ akaal_software/src/screens/MigrationModule/index.ts
@@ -2,4 +2,5 @@ export { MigrationModule } from './MigrationModule';
 export { MigrationLanding } from './MigrationLanding';
 export { ProjectWorkspaceView } from './ProjectWorkspaceView';
 export { NewMigrationConfigView } from './NewMigrationConfigView';
+export { NewMigrationWizard } from './NewMigrationWizard';
 export { NewProjectConfigView } from './NewProjectConfigView';
